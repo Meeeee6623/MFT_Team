009577ffc5679cfe8ba26fc746febc2d99bc7cca
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2023-02-06 22:48:05 -0500

    upload

--- intro_project/trader_class.py
+++ intro_project/trader_class.py
@@ -1,7 +1,16 @@
+import pandas as pd
+
+
 class Trader:
-    def __init__(self):
+    def __init__(self, n_stocks=4):
         # Add any additional info you want
-        pass
+        self.prices = pd.DataFrame({f"Stock{s}": [] for s in range(1, n_stocks + 1)})
+        # Add returns columns
+        self.prices = self.prices.append({f"Stock{s}_Returns": [] for s in range(1, n_stocks + 1)}, ignore_index=True)
+        # Add timestep column
+        self.prices = self.prices.append({"Time": []}, ignore_index=True)
+
+        self.n_stocks = n_stocks
 
     def MakeTrades(self, time, stock_prices):
         """
@@ -37,4 +46,30 @@ class SampleTrader(Trader):
                 trades['Stock2'] = 1000
             else:
                 trades['Stock2'] = -1000
+        return trades
+
+
+class MyTrader(Trader):
+
+    # given that stock3 returns closely resemble stock4 returns, trade stock4 on stock3 info
+    def MakeTrades(self, time, stock_prices):
+        # update data
+        stock_prices["Time"] = time
+        self.prices = self.prices.append(stock_prices, ignore_index=True)
+        # update returns columns by pct change of normal prices
+        for i in range(1, self.n_stocks + 1):
+            self.prices[f"Stock{i}_Returns"] = self.prices[f"Stock{i}"].pct_change()
+
+        trades = {}
+        if time > 0:
+            print(f"Stock3 returns: {self.prices['Stock3_Returns'].iloc[-1]}")
+            # when stock3 returns are positive, buy stock4
+            if self.prices["Stock3_Returns"].iloc[-1] > 0:
+                # scale proportionally to stock3 returns
+                trades["Stock4"] = 100000 * self.prices["Stock3_Returns"].iloc[-1] // 1
+                print(f"Buying stock4 at time {time} with {trades['Stock4']} shares")
+            elif self.prices["Stock3_Returns"].iloc[-1] < 0:
+                # scale proportionally to stock3 returns
+                trades["Stock4"] = -100000 * self.prices["Stock3_Returns"].iloc[-1] // 1
+                print(f"Selling stock4 at time {time} with {trades['Stock4']} shares")
         return trades
