04e6f2692ca40dcb8d569239c53ef3f94b050061
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2023-02-08 08:57:00 -0500

    new RegressionTrader -> BenjaminTrader

--- intro_project/BenjaminTrader.py
+++ intro_project/BenjaminTrader.py
@@ -1,17 +1,26 @@
-import pandas as pd
 from math import fabs
 
 
 class Trader:
     def __init__(self, n_stocks=4):
         # Add any additional info you want
-        self.prices = pd.DataFrame({f"Stock{s}": [] for s in range(1, n_stocks + 1)})
-        # Add returns columns
-        self.prices = self.prices.append({f"Stock{s}_Returns": [] for s in range(1, n_stocks + 1)}, ignore_index=True)
-        # Add timestep column
-        self.prices = self.prices.append({"Time": []}, ignore_index=True)
 
-        self.not_4 = 0
+        # last stock price data, for calculating returns
+        self.prices = {
+            "Stock1": 0.0,
+            "Stock2": 0.0,
+            "Stock3": 0.0,
+            "Stock4": 0.0
+        }
+
+        # last stock return data
+        self.returns = {
+            "Stock1_Returns": 0.0,
+            "Stock2_Returns": 0.0,
+            "Stock3_Returns": 0.0,
+            "Stock4_Returns": 0.0
+        }
+
         self.n_stocks = n_stocks
         # would read stock line data from txt files, but small enough to hardcode...
         self.regression_info = {
@@ -37,24 +46,25 @@ class Trader:
         return trades
 
 
-class RegressionTrader(Trader):
+class BenjaminTrader(Trader):
     """
     More complicated trader that uses regression lines to predict returns
     Database is leftover and mostly used for debugging, can be replaced with a single variable for each stock
     """
+
     def decide_purchase(self) -> (str, int):
         # get max return from regression line for each stock
         max_return = 0.0
         to_buy = ""
         # precdict stock1, stock2 returns with stock1 returns
         for i in range(1, 3):
-            stock_return = self.regression_info[f"Stock{i}_slope"] * self.prices[f"Stock1_Returns"].iloc[-1] + \
+            stock_return = self.regression_info[f"Stock{i}_slope"] * self.returns[f"Stock1_Returns"] + \
                            self.regression_info[f"Stock{i}_intercept"]
             if fabs(stock_return) > fabs(max_return):
                 max_return = stock_return
                 to_buy = f"Stock{i}"
         # predict stock4 returns with stock3 returns
-        stock_return = self.regression_info[f"Stock4_slope"] * self.prices[f"Stock3_Returns"].iloc[-1] + \
+        stock_return = self.regression_info[f"Stock4_slope"] * self.returns[f"Stock3_Returns"] + \
                        self.regression_info[f"Stock4_intercept"]
         if fabs(stock_return) > fabs(max_return):
             max_return = stock_return
@@ -64,22 +74,16 @@ class RegressionTrader(Trader):
 
     # given that stock3 returns closely resemble stock4 returns, trade stock4 on stock3 info
     def MakeTrades(self, time, stock_prices):
-        # update data
-        stock_prices["Time"] = time
-        self.prices = self.prices.append(stock_prices, ignore_index=True)
-        # update returns columns by pct change of normal prices
-        # was getting errors with adding only new data, so just recalculate everything
-        for i in range(1, self.n_stocks + 1):
-            self.prices[f"Stock{i}_Returns"] = self.prices[f"Stock{i}"].pct_change()
-
+        # get returns for each stock, update last stock price
+        for stock in self.prices:
+            self.returns[f"{stock}_Returns"] = (stock_prices[stock] - self.prices[stock]) / self.prices[stock]
+            self.prices[stock] = stock_prices[stock]
         trades = {}
         if time > 1:
             # buy based on regression lines
             to_buy, buy_or_sell = self.decide_purchase()
-            trades[to_buy] = buy_or_sell * 600000 // self.prices[to_buy].iloc[-1] + 1
+            trades[to_buy] = buy_or_sell * 600000 // self.prices[to_buy] + 1
             print(f"Buying {to_buy} at time {time} with {trades[to_buy]} shares")
-            if to_buy != "Stock4":
-                self.not_4 += 1
 
         return trades
 # %%
